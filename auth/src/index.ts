905af93cc3aba49db95818e5bc0bae97ea16168c
Luis Zuniga <luiszunigacr@gmail.com>
2023-12-19 16:39:47 -0600

    Added mongoose and connected to MongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import {
   currentUserRouter,
   signinRouter,
@@ -24,6 +25,23 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000. v1');
-});
+const start = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb://auth-mongo-srv:27017/auth' /*{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }*/
+    );
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000. v3');
+  });
+};
+
+start();
