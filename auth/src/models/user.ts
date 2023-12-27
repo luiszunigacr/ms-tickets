c5e02b2c0273cf7d893a1f040ddcf92a3ddbf5c6
Luis Zuniga <luiszunigacr@gmail.com>
2023-12-27 15:40:33 -0600

    Transform data returned for user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -18,16 +18,28 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
 
 // In this middleware, the second param must be a function
 // declared with the "function" keyword (not arrow function),
